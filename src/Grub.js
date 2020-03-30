46072422bd53ba78afaddbfb4e4b4722d9ee5a0f
Jorge Castello <jorgio.castello@gmail.com>
2020-03-30 11:57:50 -0700

    Implement Grub pseudo-class

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,10 @@
-var Grub = function() {
+var Grub = function(age, color) {
+  this.age = age || 0;
+  this.color = color || 'pink';
+  this.food = 'jelly';
 };
+
+Grub.prototype.eat = function() {
+  return 'Eat';
+}
 
