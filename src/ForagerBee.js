911aa0510edabd083f54aab5cc3586a9f7d67bb1
Jorge Castello <jorgio.castello@gmail.com>
2020-03-30 11:58:47 -0700

    Implement ForagerBee pseudo-class

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,11 @@
-var ForagerBee = function() {
+var ForagerBee = function(age, color, job, canFly = true) {
+  Bee.call(this, age || 10, color, job || 'find pollen');
+  this.canFly = canFly;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+}
+ForagerBee.prototype.constructor = ForagerBee;
