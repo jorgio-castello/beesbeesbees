a23b5f3c2628a343f2ff2ce6c729b18bd7ef5258
Jorge Castello <jorgio.castello@gmail.com>
2020-03-30 11:58:59 -0700

    Implement RetiredForager pseudo-class

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,12 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this, 40, 'grey','gamble', false)
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.forage = function() {
+  return 'I am too old, let me play cards instead';
+}
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+}
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
