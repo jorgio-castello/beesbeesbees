ecb9944772fb4dc48d03a1341a9bb4c624c78eac
Jorge Castello <jorgio.castello@gmail.com>
2020-03-30 11:58:08 -0700

    Implement Bee pseudo-class

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,8 @@
-var Bee = function() {
+var Bee = function(age, color, job) {
+  Grub.call(this, age || 5, color || 'yellow');
+  this.job = job || 'keep on growing';
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
 
