ace2f8f4136b749c1262f3a587e8e94c3e08deb8
Jorge Castello <jorgio.castello@gmail.com>
2020-03-30 11:58:25 -0700

    Implement HoneyMakerBee pseudo-class

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,13 @@
 var HoneyMakerBee = function() {
+  Bee.call(this, 10, undefined, 'make honey');
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+}
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
